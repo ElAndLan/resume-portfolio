06619fce63027673f93bfc4a362a284e6c9309fd
Andrew LaNoue Personal <alanoue@catalyte.io>
2022-10-28 21:08:53 -0400

    Push toa ttempt to fix heroku bug

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -43,7 +43,7 @@ const Home = () => {
           <span className={`${letterClass} _12`}>i,</span>
           <br />
           <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m,</span>
+          <span className={`${letterClass} _14`}>'m</span>
 
           <img src={LogoTitle} alt="developer" />
           <AnimatedLetters
